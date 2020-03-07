4a9a466802ecc703ebbca0b0d1d5232f4f8d9ea3
guilhermesurek <guilhermesurek@gmail.com>
2020-03-07 15:52:36 -0300

    Aula8 - Model Admin e Forms

---
+++ aula8/admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from .models import Pet
+from .forms import PetForm
+
+# Register your models here.
+
+class PetAdmin(admin.ModelAdmin):
+    fields = ('nome', 'data_nascimento', 'ativo')
+    list_display = ('nome', 'data_nascimento', 'show_and_year', 'ativo')
+    list_filter = ('ativo',)
+    search_fields = ('nome',)
+    form = PetForm
+
+    def show_and_year(self, obj):
+        return f"{obj.nome} {obj.data_nascimento}"
+    
+    def starts_with_b(self, obj):
+        return obj.nome.startswith('b')
+
+admin.site.register(Pet, PetAdmin)
